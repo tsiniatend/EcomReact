c37571816decb8220de9a66e78e859da40e270fa
tsiniate endalkachew <kesuendalk@gmail.com>
2022-07-24 22:37:48 -0400

    ok

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const cors = require ("cors");
 const app = express();
+const mysql = require("mysql");
 const port = process.env.PORT || 8080; 
 
 app.use(cors());
