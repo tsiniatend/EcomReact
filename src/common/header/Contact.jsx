6c7679735fe063aaab302d0825335990d4b67cdf
tsiniate endalkachew <kesuendalk@gmail.com>
2022-07-27 12:12:14 -0400

    ok

--- src/common/header/Contact.jsx
+++ src/common/header/Contact.jsx
@@ -7,15 +7,16 @@ const Contact = () => {
 <div className="container2">
     <div className="contact-box">
       <form method="post" action="https://ecomhost.herokuapp.com/contact" >
-        <input type="text" className="input-field" placeholder="Your Name" name="name" />
-        <input type="email" className="input-field" placeholder="Your Email" name="email"/>
-        <input type="text" className="input-field" placeholder="Phone Number" name="phone"/>
+        <input type="text" className="input-field" placeholder="Your Name" name="name" id="name"/>
+        <input type="email" className="input-field" placeholder="Your Email" name="email" id="email"/>
+        <input type="text" className="input-field" placeholder="Phone Number" name="phone" id="phone"/>
         <textarea
           type="text"
           className="input-field textarea-field"
           placeholder="Your message"
           defaultValue={""}
           name="message"
+          id="message"
         />
         <button type="sumbit" value="send" className="send">
           Send Message
